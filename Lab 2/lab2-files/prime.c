0d89a5568f2bd01a1b55618c2c12264e7b8372ad
Sana <144557568+Sana8200@users.noreply.github.com>
2025-09-16 10:18:36 +0200

    Completed prime function

--- Lab 2/lab2-files/prime.c
+++ Lab 2/lab2-files/prime.c
@@ -9,28 +9,45 @@
 #include <stdio.h>
 
 int is_prime(int n){
+
   if (n <= 1) {
-    return 0;    // Numbers less than or equal to 1 are not prime.
+    return 0;          // Numbers less than or equal to 1 are not prime.
   }
   
   if (n == 2){
-    return 1;    // 2 is the only even prime number.
+    return 1;          // 2 is the only even prime number.
   }
 
   if (n % 2 == 0){
-    return 0;    // Other even numbers are not prime.
+    return 0;          // Other even numbers larger than 2 are not prime. 
   }
-  
-  for (int i = 2; i * i <= n; i++) {    // Check divisors from 2 to sqrt(n)
-     if (n % i == 0) {
-      return 0;  // Found a divisor, so n is not prime.
-  
-    return 0;
+ 
+  for (int i = 1; i < n; i += 2) {       // Checking odd numbers larger than 2 
+     if (n % i == 0) {                   // If n is divisible by i, it is not prime.
+      return 0;
+     }
+  }
+
+/*
+for (int i = 3; i * i <= n; i += 2) {           // Check only odd numbers from 3 to sqrt(n), more efficent 
+                                                // No need to check even numbers or numbers greater than sqrt(n)
+    if (n % i == 0) {
+        return 0; // n is divisible by i, so it is not prime
+    }
 }
+*/
+
+  return 1;                              // If no divisors were found, the number is prime.
+}
+
+
 int main(void){
   printf("%d\n", is_prime(11));  // 11 is a prime.      Should print 1.
   printf("%d\n", is_prime(383)); // 383 is a prime.     Should print 1.
   printf("%d\n", is_prime(987)); // 987 is not a prime. Should print 0.
   return 0;
 }
+
+
+
 
