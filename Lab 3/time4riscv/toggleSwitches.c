282fe4f0b9abb21380210aef75367ec79c1acd1d
Sana <144557568+Sana8200@users.noreply.github.com>
2025-10-01 17:58:23 +0200

    This file contains get_sw(void) function which reads the toggle switches status

---
+++ Lab 3/time4riscv/toggleSwitches.c
@@ -0,0 +1,28 @@
+/*
+ * int get_sw(void);
+ * Reads the status of the 10 toggle switches on the board 
+ * no parameter 
+ * the return value will be an integer where 10 least significant bits correspond to the state of the switches SW0-SW9 
+ * All other bits should be 0 
+ */
+
+#define SWITCH_ADDR  0x04000010
+
+
+int get_sw(void){
+
+    // Creating a volatile pointer to the switch hardware address
+    volatile int *switch_pointer = (volatile int *) SWITCH_ADDR;
+
+    int switch_value = *switch_pointer;   // Deferencing, and just getting the value at the switch address and putting it into the new variable switch_value
+
+    // Creating a mask (masking operation), isolating the 10 least significant bits 
+    // 0x3FF = 0011 1111 1111
+    int mask = 0x3FF;
+
+    // Applying the mask using & bitwise AND operation, all bits other than 10 least significant bits will be 0 
+    int result = switch_value & mask;
+
+    return result; 
+}
+
