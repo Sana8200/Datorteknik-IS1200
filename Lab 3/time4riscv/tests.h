e1ce59a6bcc146746bf9febed9dc3b3fa1e5915c
Sana <144557568+Sana8200@users.noreply.github.com>
2025-10-03 04:49:31 +0200

    Header file for hardware testt

---
+++ Lab 3/time4riscv/tests.h
@@ -0,0 +1,21 @@
+
+/* tests.h
+   Header file for hardware tests
+*/
+
+#ifndef TESTS_H
+#define TESTS_H
+
+// Runs the initial 16-second LED startup sequence.
+void run_start_sequence(void);
+
+// Declares the function for the switch and LED test loop.
+void switch_button_test(void);
+
+
+// --- External Global Variables ---
+// These are DEFINED in labmain.c and declared here for access by other files.
+extern int mytime;
+extern char textstring[];
+
+#endif /* TESTS_H */
